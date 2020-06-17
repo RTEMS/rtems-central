86dfe76f384ed992301bfb1a51a17439ebc9121a
Sebastian Huber <sebastian.huber@embedded-brains.de>
2020-06-17 09:12:28 +0200

    validation: Be in line with the specification

--- rtemsqual/validation.py
+++ rtemsqual/validation.py
@@ -667,7 +667,7 @@ def _gather_items(item: Item, source_files: Dict[str, _SourceFile],
     elif item["type"] == "requirement" and item[
             "requirement-type"] == "functional" and item[
                 "functional-type"] == "action":
-        src = _get_source_file(item["test-target-source"], source_files)
+        src = _get_source_file(item["test-target"], source_files)
         src.add_test_directive(item)
     elif item["type"] == "build" and item["build-type"] == "test-program":
         test_programs.append(_TestProgram(item))
